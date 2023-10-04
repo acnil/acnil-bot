bb6d51154dfcd89f646ace32c36790cca029724c
metalblueberry <metalblueberry@gmail.com>
2023-10-04 16:51:00 +0200

    ensure binaries are always the same

--- tf/apply.sh
+++ tf/apply.sh
@@ -3,9 +3,9 @@
 set -e 
 set -o pipefail
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -tags lambda.norpc -o cmd/lambda/package/bootstrap cmd/lambda/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -trimpath -tags lambda.norpc -o cmd/lambda/package/bootstrap cmd/lambda/main.go
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -tags lambda.norpc -o cmd/auditLambda/package/bootstrap cmd/auditLambda/main.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -trimpath -tags lambda.norpc -o cmd/auditLambda/package/bootstrap cmd/auditLambda/main.go
 
 terraform -chdir=./tf apply -input=false --auto-approve \
      -var=sheets_private_key="$SHEETS_PRIVATE_KEY" \
