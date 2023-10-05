8f5293991ff4be8990baf4d64ac2a5ddb7ae7c82
metalblueberry <metalblueberry@gmail.com>
2023-10-05 12:31:49 +0200

    print checksums

--- tf/apply.sh
+++ tf/apply.sh
@@ -4,16 +4,22 @@ set -e
 set -o pipefail
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -trimpath -tags lambda.norpc -o cmd/lambda/package/bootstrap cmd/lambda/main.go
+echo Binary MD5 $(cat cmd/lambda/package/bootstrap | md5sum )
 ## Why?
 ## https://zerostride.medium.com/building-deterministic-zip-files-with-built-in-commands-741275116a19
-chmod 777 cmd/lambda/package/bootstrap
+# chmod 777 cmd/lambda/package/bootstrap
 touch cmd/lambda/package/bootstrap -t 201301250000
 (cd cmd/lambda/package; rm -f ../package.zip; zip -rq -D -X -9 -A --compression-method deflate ../package.zip bootstrap;)
 
+echo Zip MD5 $(cat cmd/lambda/package.zip | md5sum )
+
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -trimpath -tags lambda.norpc -o cmd/auditLambda/package/bootstrap cmd/auditLambda/main.go
-chmod 777 cmd/auditLambda/package/bootstrap
+echo Binary MD5 $(cat cmd/auditLambda/package/bootstrap | md5sum )
+# chmod 777 cmd/auditLambda/package/bootstrap
 touch cmd/auditLambda/package/bootstrap -t 201301250000
 (cd cmd/auditLambda/package; rm -f ../package.zip; zip -rq -D -X -9 -A --compression-method deflate ../package.zip bootstrap;)
+
+echo Zip MD5 $(cat cmd/auditLambda/package.zip | md5sum )
 
 terraform -chdir=./tf apply -input=false --auto-approve \
      -var=sheets_private_key="$SHEETS_PRIVATE_KEY" \
