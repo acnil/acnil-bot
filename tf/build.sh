7e138bc4c64c05c4bcae282f65cb4ebb882c75d4
metalblueberry <metalblueberry@gmail.com>
2023-10-05 18:21:17 +0200

    simplfy build script

--- tf/build.sh
+++ tf/build.sh
@@ -3,29 +3,31 @@
 set -e 
 set -o pipefail
 
+build () {
+## Some instructions to make zip files always the same
+## https://zerostride.medium.com/building-deterministic-zip-files-with-built-in-commands-741275116a19
 export CGO_ENABLED=0 
 export GOOS=linux 
 export GOARCH=amd64
 export GOPATH=/home/runner/go
 
-build="go build -trimpath -tags lambda.norpc -buildvcs=true -compiler gc"
+echo Building $1
+(
+    cd $1;
+    go build -trimpath -tags lambda.norpc -buildvcs=true -compiler gc -o package/bootstrap;
+    chmod 777 package/bootstrap
+    echo Binary MD5 $(cat package/bootstrap | md5sum )
+    touch package/bootstrap -t 201301250000
+)
+(
+    cd $1/package;
+    rm -f ../package.zip;
+    zip -rq -D -X -9 -A --compression-method deflate ../package.zip bootstrap;
+    echo Zip MD5 $(cat ../package.zip | md5sum )
+)
+}
+
 go env
 
-(cd cmd/lambda; $build -o package/bootstrap)
-
-chmod 777 cmd/lambda/package/bootstrap
-echo Binary MD5 $(cat cmd/lambda/package/bootstrap | md5sum )
-## Why?
-## https://zerostride.medium.com/building-deterministic-zip-files-with-built-in-commands-741275116a19
-touch cmd/lambda/package/bootstrap -t 201301250000
-(cd cmd/lambda/package; rm -f ../package.zip; zip -rq -D -X -9 -A --compression-method deflate ../package.zip bootstrap;)
-
-echo Zip MD5 $(cat cmd/lambda/package.zip | md5sum )
-
-(cd cmd/auditLambda; $build -o package/bootstrap)
-chmod 777 cmd/auditLambda/package/bootstrap
-echo Binary MD5 $(cat cmd/auditLambda/package/bootstrap | md5sum )
-touch cmd/auditLambda/package/bootstrap -t 201301250000
-(cd cmd/auditLambda/package; rm -f ../package.zip; zip -rq -D -X -9 -A --compression-method deflate ../package.zip bootstrap;)
-
-echo Zip MD5 $(cat cmd/auditLambda/package.zip | md5sum )
+build cmd/lambda
+build cmd/auditLambda
