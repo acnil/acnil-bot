e23e41d8489d0f1315f86be2c8659b50dafa9d3f
metalblueberry <metalblueberry@gmail.com>
2023-10-05 13:10:53 +0200

    change order

--- tf/build.sh
+++ tf/build.sh
@@ -13,18 +13,18 @@ go env
 
 $flags -o cmd/lambda/package/bootstrap cmd/lambda/main.go
 
+chmod 777 cmd/lambda/package/bootstrap
 echo Binary MD5 $(cat cmd/lambda/package/bootstrap | md5sum )
 ## Why?
 ## https://zerostride.medium.com/building-deterministic-zip-files-with-built-in-commands-741275116a19
-chmod 777 cmd/lambda/package/bootstrap
 touch cmd/lambda/package/bootstrap -t 201301250000
 (cd cmd/lambda/package; rm -f ../package.zip; zip -rq -D -X -9 -A --compression-method deflate ../package.zip bootstrap;)
 
 echo Zip MD5 $(cat cmd/lambda/package.zip | md5sum )
 
 $flags -o cmd/auditLambda/package/bootstrap cmd/auditLambda/main.go
-echo Binary MD5 $(cat cmd/auditLambda/package/bootstrap | md5sum )
 chmod 777 cmd/auditLambda/package/bootstrap
+echo Binary MD5 $(cat cmd/auditLambda/package/bootstrap | md5sum )
 touch cmd/auditLambda/package/bootstrap -t 201301250000
 (cd cmd/auditLambda/package; rm -f ../package.zip; zip -rq -D -X -9 -A --compression-method deflate ../package.zip bootstrap;)
 
