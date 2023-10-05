de066b798307be05a0f06edb43423acae882ead5
metalblueberry <metalblueberry@gmail.com>
2023-10-05 13:10:19 +0200

    try set permissions

--- tf/build.sh
+++ tf/build.sh
@@ -16,7 +16,7 @@ $flags -o cmd/lambda/package/bootstrap cmd/lambda/main.go
 echo Binary MD5 $(cat cmd/lambda/package/bootstrap | md5sum )
 ## Why?
 ## https://zerostride.medium.com/building-deterministic-zip-files-with-built-in-commands-741275116a19
-# chmod 777 cmd/lambda/package/bootstrap
+chmod 777 cmd/lambda/package/bootstrap
 touch cmd/lambda/package/bootstrap -t 201301250000
 (cd cmd/lambda/package; rm -f ../package.zip; zip -rq -D -X -9 -A --compression-method deflate ../package.zip bootstrap;)
 
@@ -24,7 +24,7 @@ echo Zip MD5 $(cat cmd/lambda/package.zip | md5sum )
 
 $flags -o cmd/auditLambda/package/bootstrap cmd/auditLambda/main.go
 echo Binary MD5 $(cat cmd/auditLambda/package/bootstrap | md5sum )
-# chmod 777 cmd/auditLambda/package/bootstrap
+chmod 777 cmd/auditLambda/package/bootstrap
 touch cmd/auditLambda/package/bootstrap -t 201301250000
 (cd cmd/auditLambda/package; rm -f ../package.zip; zip -rq -D -X -9 -A --compression-method deflate ../package.zip bootstrap;)
 
